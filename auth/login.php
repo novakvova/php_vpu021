86b6d9dd69cfa16e1e9e87b1e5f408842f79b03e
novakvova <novakvova@gmail.com>
2022-06-12 11:41:28 +0300

    Begin login page

--- auth/login.php
+++ auth/login.php
@@ -1,5 +1,18 @@
 <?php include $_SERVER['DOCUMENT_ROOT']."/_haed.php" ?>
-
     <h1 class="text-center">Вхід на сайт</h1>
+    <div class="row">
+        <form class="offset-md-3 col-md-6">
+            <div class="mb-3">
+                <label for="email" class="form-label">Пошта</label>
+                <input type="email" class="form-control" id="email" name="email">
+            </div>
 
+            <div class="mb-3">
+                <label for="password" class="form-label">Пароль</label>
+                <input type="password" class="form-control" id="password" name="password">
+            </div>
+
+            <button type="submit" class="btn btn-primary">Війти</button>
+        </form>
+    </div>
 <?php include $_SERVER['DOCUMENT_ROOT']."/_footer.php"; ?>
